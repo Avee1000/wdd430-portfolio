fb044981fcb579cb0b912efb95314004eb4fdea8
Avee1000 <eddyidahosa01@gmail.com>
2026-07-04 19:08:31 +0000

    adafd

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {  
   return (
-    <footer className="border-t border-gray-200 g-black-800 text-white py-4">
+    <footer className="bg-black border-t border-gray-200 g-black-800 text-white py-4">
       <div className="container mx-auto text-center">
         <p>Copyright &copy; {new Date().getFullYear()} | OFI-S | All rights reserved</p>
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ const navItems = [
 
 export default function Header() {
     return (
-        <header className="border-b border-gray-200 bg-black shadow-sm">
+        <header className="border-b border-gray-200 bg-black shadow-sm text-gray-100" >
             <div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
 
                 <Link
